ecfd6ba2ecf11c74b335fd3862d61a6a7311ef68
alik-jc <jesusdaniel723@gmail.com>
2025-12-07 16:12:06 -0500

    feat: Add Vercel rewrite and SSR API endpoint, create version file, and remove vite-plugin-vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
-// @ts-ignore
-import vercel from 'vite-plugin-vercel'
 
 export default defineConfig({
-  plugins: [react(), ssr(), vercel()],
+  plugins: [react(), ssr()],
   server: {
     port: 3000,
   },
